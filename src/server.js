500b5046cf412403b7aca64f945508fedffb22be
mossab <mossabchb230@gmail.com>
2026-08-31 19:13:05 -0800

    laste version just url config

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
+const Url_host = process.env.Url_host ;
 
 app.listen(PORT, () => {
-  console.log(`🚀 LMS Auth backend running on http://localhost:${PORT}`);
+  console.log(`🚀 LMS Auth backend running on ${Url_host}`);
 });
